4d4f9c61588b9576b44415002fd0f6b6183cc260
Bruno Ribeiro <brunz36@gmail.com>
2017-04-27 19:22:07 -0400

    Searched Working

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// $('#query').on('input', function(event) {
+//   let queryValue = $(this).val()
+//
+//   console.log(`You are searching for ${queryValue}`)
+//
+//   $.ajax({
+//     url: '/items',
+//     data: { query: queryValue },
+//     dataType: 'script'
+//   })
+// })
